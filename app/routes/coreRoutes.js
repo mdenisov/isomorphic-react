118830f5d0c332c95d5667539a3968b0383c6a0d
Maxim Denisov <m.denisov@banki.ru>
2016-02-04 17:40:19 +0300

    Bug fixing

--- app/routes/coreRoutes.js
+++ app/routes/coreRoutes.js
@@ -7,7 +7,7 @@ module.exports = function(app) {
 	app.get('/', function(req, res){
 		// React.renderToString takes your component
 	  // and generates the markup
-		var reactHtml = ReactDOMServer.renderToString(<ReactApp />;
+		var reactHtml = ReactDOMServer.renderToString(<ReactApp />);
 	  // Output html rendered by react
 		// console.log(myAppHtml);
     res.render('index.ejs', {reactOutput: reactHtml});
